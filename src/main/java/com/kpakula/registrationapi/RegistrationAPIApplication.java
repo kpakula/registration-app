80f15ab9dd3298191344b31f64ec79c0d25a9610
Konrad Pakula <konrad.a.pakula@gmail.com>
2022-01-22 21:22:50 +0100

    Initial commit

---
+++ src/main/java/com/kpakula/registrationapi/RegistrationAPIApplication.java
@@ -0,0 +1,13 @@
+package com.kpakula.registrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationAPIApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kpakula/registrationapi/RegistrationAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpakula.registrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAPIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
